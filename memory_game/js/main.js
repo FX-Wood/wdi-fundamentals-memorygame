27362e9c79ae3135a820e83b263a986cee93dfa1
FX-Wood <fxwood2@gmail.com>
2019-01-07 17:02:45 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree)
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne)
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo)
+
+console.log(cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[1] === cardsInPlay[2]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again")
+		cardsInPlay = []
+	}
+
+}
