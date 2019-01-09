d9b803137a9499dbc806559d030638a1918baccb
FX-Wood <fxwood2@gmail.com>
2019-01-08 22:42:20 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,43 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId])
+	console.log("User flipped " + cards[cardId].rank)
 
+	cardsInPlay.push(cards[cardId].rank);
 
-	let cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	console.log("User flipped " + cardOne)
-
-	let cardTwo = cards[2];
-	cardsInPlay.push(cardTwo);
-	console.log("User flipped " + cardTwo)
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 
 	checkForMatch()
 }
 
 flipCard(0)
+flipCard(2)
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
@@ -29,6 +47,5 @@ function checkForMatch() {
 		alert("Sorry, try again")
 		cardsInPlay = []
 	}
-
 }
 }
