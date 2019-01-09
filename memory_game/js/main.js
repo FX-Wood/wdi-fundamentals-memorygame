362be5ef5ac8a5fde334f82d4fcf3499d2a2d2a3
FX-Wood <fxwood2@gmail.com>
2019-01-09 00:27:27 -0800

    added finishing touches

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,7 @@
 console.log("Up and running!");
 
+var score = 0;
+
 var cards = [
 {
 	rank: "queen",
@@ -28,44 +30,57 @@ function createBoard() {
 		let cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
-		cardElement.addEventListener('click', flipCard)
-		document.querySelector('#game-board').appendChild(cardElement)
+		cardElement.addEventListener('click', flipCard);
+		document.querySelector('#game-board').appendChild(cardElement);
 	}
 }
 
 var cardsInPlay = [];
 
 function flipCard() {
-	let cardId = this.getAttribute('data-id')
+	let cardId = this.getAttribute('data-id');
 
-	console.log("User flipped " + cards[cardId].rank)
+	console.log("User flipped " + cards[cardId].rank);
 
 	cardsInPlay.push(cards[cardId].rank);
 
-	this.setAttribute('src', cards[cardId].cardImage)
+	this.setAttribute('src', cards[cardId].cardImage);
 
-	setTimeout(checkForMatch, 200)
+	setTimeout(checkForMatch, 200);
 }
 
 function clearBoard() {
 	document.querySelectorAll('img').forEach(function(element) {
-		element.setAttribute('src', 'images/back.png')
+		element.setAttribute('src', 'images/back.png');
 	})
 	cardsInPlay = []
+}
+
+function incrementScore() {
+	if (!document.querySelector('#scoreBox')) {
+		var scoreBox = document.createElement('div');
+		scoreBox.setAttribute('id', 'scoreBox')
+		scoreBox.textContent = "Score: " + (score += 1)
+		document.querySelector('#game-board').appendChild(scoreBox)
+
+	} else {
+		document.querySelector('#scoreBox').textContent = "Score: " + (score += 1)
+	}
 }
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
-			clearBoard()
+			clearBoard();
+			incrementScore()
 		} else {
-			alert("Sorry, try again")
-			cardsInPlay = []
-			clearBoard()
+			alert("Sorry, try again");
+			cardsInPlay = [];
+			clearBoard();
 		}
 	}
 }
 
 
-createBoard()
+createBoard();
